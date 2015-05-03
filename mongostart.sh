7aa49e32a9227346136078459422e7c2c6266a95
derickson <David.Erickson@gmail.com>
2015-05-03 12:17:17 -0400

    mongo env setup

--- mongostart.sh
+++ mongostart.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir -p data/standalone
+mkdir -p data/standalone logs
 
 ## Standard
-mongod --logpath standalone.log --dbpath data/standalone --smallfiles --fork --port 27017 --oplogSize 64
+mongod --logpath logs/standalone.log --dbpath data/standalone --smallfiles --fork --port 27017 --oplogSize 64
 
 ## WiredTiger in 2.8
 ## $WHICHMONGOD --logpath standalone.log --dbpath data/standalone --smallfiles --fork --port 27017 --storageEngine wiredtiger --oplogSize 64
